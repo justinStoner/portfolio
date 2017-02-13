c03e21603ea3b800a1efc027491a6220fa114c67
justin <jaystoned@me.com>
2017-02-12 16:45:18 -0800

    ui changes

--- src/showcases/beatmaker/components/knob.js
+++ src/showcases/beatmaker/components/knob.js
@@ -13,7 +13,45 @@ export class KnobCustomElement{
   attached(){
     console.log(this.element.children);
     $(this.element.children[0]).knob({
-      'change':(v)=>{console.log(this.val=v)}
+      'change':(v)=>{console.log(this.val=v)},
+      'displayPrevious':true,
+      'draw':function () {
+                this.cursorExt = 0.3;
+
+                var a = this.arc(this.cv),
+                pa,
+                r = 1,
+                sx=2,
+                sy=2;
+                this.g.lineWidth = this.lineWidth;
+                console.log(this);
+                this.$div[0].lastChild.style.marginLeft-=sy;
+                if (this.o.displayPrevious) {
+                    pa = this.arc(this.v);
+                    this.g.beginPath();
+                    this.g.strokeStyle = this.pColor;
+                    this.g.shadowColor='transparent';
+                    this.g.arc(this.xy-sx, this.xy-sy, this.radius-(sx+sy), pa.s, pa.e, pa.d);
+                    this.g.stroke();
+                }
+                this.g.beginPath();
+                this.g.strokeStyle = r ? this.o.bgColor : this.bgColor ;
+                this.g.arc(this.xy-sx, this.xy-sy, this.radius-(sx+sy), 0, 2*Math.PI, a.d);
+                this.g.shadowColor='rgba(0, 0, 0, 0.15)';
+                this.g.shadowBlur = 2;
+                this.g.shadowOffsetX = sx;
+                this.g.shadowOffsetY = sy;
+                this.g.stroke();
+
+                this.g.beginPath();
+                this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+                this.g.shadowColor='transparent';
+                this.g.arc(this.xy-sx, this.xy-sy, this.radius-(sx+sy), a.s, a.e, a.d);
+                this.g.stroke();
+
+                return false;
+
+        }
     });
     //this.element.children[1].addEventListener('changed')
   }
