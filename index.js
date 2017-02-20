73968e72ebf1e4f309a9542ffadad5f1177a3946
justin <jaystoned@me.com>
2017-02-20 14:35:39 -0800

    gzip compression

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var app=require('express')();
 var server=require('http').createServer(app);
 var express=require('express');
+var compression = require('compression');
+
+app.use(compression());
+
 app.use(express.static(__dirname))
 
-app.listen(80);
+app.listen(5000);
