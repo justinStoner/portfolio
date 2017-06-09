3086fe86917ea76d89197ff55d89730f53905a28
justin <jaystoned@me.com>
2017-06-09 11:29:50 -0700

    navigation

--- src/resources/elements/side-nav/side-nav.js
+++ src/resources/elements/side-nav/side-nav.js
@@ -9,5 +9,16 @@ export class SideNav {
     this.appRouter=appRouter;
     this.ds=ds;
     this.ab=ab;
+    var testExp = new RegExp('Android|webOS|iPhone|iPad|' + 'BlackBerry|Windows Phone|'  + 'Opera Mini|IEMobile|Mobile' , 'i');
+    this.isMobile=testExp.test(navigator.userAgent);
+  }
+  attached(){
+    this.container=document.querySelector('app-container');
+  }
+  closeDrawer(href){
+    if(this.isMobile){
+      document.querySelector('.mdl-layout__obfuscator').click()
+    }
+    this.appRouter.router.navigate(href);
   }
 }
